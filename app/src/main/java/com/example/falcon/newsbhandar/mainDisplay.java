96322102c8d74f0dc1d99effdd5ec03b7fc00aa1
KaustubhKekre <kkekre90@gmail.com>
2019-01-06 01:46:02 +0530

    basic google sign in done(using checking at onStart)

---
+++ app/src/main/java/com/example/falcon/newsbhandar/mainDisplay.java
@@ -0,0 +1,13 @@
+package com.example.falcon.newsbhandar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class mainDisplay extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_display);
+
+
+    }
+}
